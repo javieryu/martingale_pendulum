904e75970ea409d943babd7c112bbacf578e17c4
javier <javiercyu@gmail.com>
2023-05-10 16:14:23 -0700

    start dynamics

--- simulator.py
+++ simulator.py
@@ -4,3 +4,16 @@ import numpy as np
 Simulator related code goes in here.
 """
 
+def simulate_pendulum(x_init, T, length, config):
+    """
+    Takes initial conditions and rolls out the dynamics for a fixed time frame.
+    x_init: state at t = 0, [theta (radians), theta_d (radians / s)] 
+    T: duration of simulation in seconds
+    config
+    """
+
+    return 
+
+def pendulum_dynamics(x, t):
+    return
+
